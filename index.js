0302277abb089f1557a8033bbeb00dee98d6a1af
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-27 02:52:32 +0600

    when add to card clicked , it will post to mongodb

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7orfsex.mongodb.net/?retryWrites=true&w=majority`;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,6 +29,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
 
 
     app.get("/menu", async (req, res) => {
@@ -38,6 +40,15 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
+    // carts collection when add to card clicked , it will post to mongodb 
+
+    app.post("/carts", async (req, res) => {
+      const item  = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
