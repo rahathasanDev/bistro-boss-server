3e5a7f0d50f1bddd47693991f432ae75f9f58ab6
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-03 12:50:57 +0600

    Send JWT token to back end for verification

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const verifyJWT = (req,res,next) =>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'})
+  }
+  // bearer token 
+  const token = authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message:'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7orfsex.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -88,6 +105,10 @@ async function run() {
       console.log(email);
       if (!email) {
         return res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error:true,message:'forbidden access'})
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
